f36b77c4e71507cb30213da7a4f654326bc26fb8
MichaelWatts-EHS <michael.watts@massmail.state.ma.us>
2021-09-21 12:04:03 -0400

    Add files via upload

---
+++ Ask-Who.ps1
@@ -0,0 +1,4 @@
+﻿$msgYes     = New-Object System.Management.Automation.Host.ChoiceDescription " &Yes ", "Yes"
+$msgNo      = New-Object System.Management.Automation.Host.ChoiceDescription " &No ", "No"
+$msgOptions = [System.Management.Automation.Host.ChoiceDescription[]]($msgYes, $msgNo)
+$askupdates = $host.ui.PromptForChoice("Who is that?", "Is that you AB?", $msgOptions, 0) 
