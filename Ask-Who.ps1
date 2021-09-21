9d0d5713a2522f41cf51a664a329b424711541dd
MichaelWatts-EHS <michael.watts@massmail.state.ma.us>
2021-09-21 12:45:32 -0400

    Add files via upload

--- Ask-Who.ps1
+++ Ask-Who.ps1
@@ -1,4 +1,12 @@
-﻿$msgYes     = New-Object System.Management.Automation.Host.ChoiceDescription " &Yes ", "Yes"
+﻿Clear-Host
+$msgYes     = New-Object System.Management.Automation.Host.ChoiceDescription " &Yes ", "Yes"
 $msgNo      = New-Object System.Management.Automation.Host.ChoiceDescription " &No ", "No"
 $msgOptions = [System.Management.Automation.Host.ChoiceDescription[]]($msgYes, $msgNo)
-$askupdates = $host.ui.PromptForChoice("Who is that?", "Is that you AB?", $msgOptions, 0) 
+$question = $host.ui.PromptForChoice("Who dat?", "I'm just a computer, it's not like I have eyes.`nIs that you AB?", $msgOptions, 0)
+
+If ($question -eq 0) {
+$file = "https://raw.githubusercontent.com/MichaelWatts-EHS/Example4AB/master/tada.wav"
+$PlayWav=New-Object System.Media.SoundPlayer
+$PlayWav.SoundLocation=$file
+$PlayWav.playsync()
+}
---
+++ Get-Git.ps1
@@ -0,0 +1,11 @@
+﻿# powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -Command "& { & '.\Get-Git.ps1' -ConfigFile Filename.xml; Exit $LastExitCode }"
+
+
+$dlClient = New-Object System.Net.WebClient
+$ghRoot = "https://raw.githubusercontent.com/MichaelWatts-EHS/Example4AB/master"
+If (!(Test-Path "$PSScriptRoot\Example4AB")) {New-Item -Path $PSScriptRoot -Name 'Example4AB' -ItemType Directory | Out-Null}
+$arrMisc = [System.Collections.ArrayList] @("FTWCMLog64.dll","FTWldap64.dll","UI++.exe")
+ForEach ($Item in $arrMisc) {If (!(Test-Path "$PSScriptRoot\Example4AB\$Item"))   {$dlClient.DownloadFile("$ghRoot/$Item", "$PSScriptRoot\Example4AB\$Item")}}
+
+[string]$AskPS = "$ghRoot/Ask-Who.ps1"
+. $moduleMain
